793e1e237d0529949815105f07b262f76ce2fbb9
leandrojamir <leandrojamir@hotmail.com>
2022-06-01 14:41:47 -0300

    adcionando exercicios

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/questao7.js
@@ -0,0 +1,19 @@
+// 7 - Escreva um teste que verifique a chamada do callback de uma função uppercase, que transforma as 
+// letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
+
+const uppercase = (str, callback) => {
+    setTimeout(() => {
+      callback(str.toUpperCase());
+    }, 500);
+  };
+  
+it('testa se com uppercase abcd fica igual ABCD', (done) => {
+  uppercase('abcd', (str) => {
+    try {
+      expect(str).toEqual('ABCD');
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
