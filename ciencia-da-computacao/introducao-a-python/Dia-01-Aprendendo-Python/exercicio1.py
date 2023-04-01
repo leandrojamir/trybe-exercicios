e9d5b33561a6ee0cb02f1a30483d4b03e91569f8
Leandro Jamir Silva <leandrojamir@hotmail.com>
2023-04-01 11:32:09 -0300

    Revert "feat: exercicio1"

--- ciencia-da-computacao/introducao-a-python/Dia-01-Aprendendo-Python/exercicio1.py
+++
@@ -1,6 +0,0 @@
-# Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
-
-def bigger(number, otherNumber):
-    if otherNumber > number:
-        return otherNumber
-    return number
