49345949bd3cbc0f4f444838e85bc3707b3fd4c1
leandrojamir <leandrojamir@hotmail.com>
2023-04-10 11:40:02 -0300

    feat: exercicio5 complete

--- ciencia-da-computacao/Padrões-de-Projeto/Dia-02-Padrões-Iterator-Adapter-Strategy/exercicio5.py
+++ ciencia-da-computacao/Padrões-de-Projeto/Dia-02-Padrões-Iterator-Adapter-Strategy/exercicio5.py
@@ -8,6 +8,24 @@
 # Dica: Se você garantir que a imagem SVG seja renderizada utilizando a
 # mesma interface que a imagem PNG, a imagem se tornará compatível.
 
+# from abc import ABC, abstractmethod
+# class PngInterface(ABC):
+#     @abstractmethod
+#     def draw(self):
+#         raise NotImplementedError
+# class PngImage(PngInterface):
+#     def __init__(self, png_path):
+#         self.png_path = png_path
+#         self.format = "raster"
+#     def draw(self):
+#         print(f"Drawing PNG {self.png_path} with {self.format}")
+# class SvgImage:
+#     def __init__(self, svg_path):
+#         self.svg_path = svg_path
+#         self.format = "vector"
+#     def get_image(self):
+#         return f"SVG {self.svg_path} with {self.format}"
+
 from abc import ABC, abstractmethod
 
 
@@ -32,4 +50,12 @@ class SvgImage:
         self.format = "vector"
 
     def get_image(self):
-        return f"SVG {self.svg_path} with {self.format}"
+        return f"SVG {self.png_path} with {self.format}"
+
+
+class SvgAdapter(PngInterface):
+    def __init__(self, svg):
+        self.svg = svg
+
+    def draw(self):
+        print(f"Drawing {self.svg.get_image()}")
