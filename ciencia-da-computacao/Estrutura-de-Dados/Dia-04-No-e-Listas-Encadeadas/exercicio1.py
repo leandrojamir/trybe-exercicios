40ea4fd14cd9e6008fd3ae734e5f76849e203556
leandrojamir <leandrojamir@hotmail.com>
2023-05-15 19:16:42 -0300

    docs: exercicio1

---
+++ ciencia-da-computacao/Estrutura-de-Dados/Dia-04-No-e-Listas-Encadeadas/exercicio1.py
@@ -0,0 +1,19 @@
+# Exercício 1:
+#  Aprimorando a classe Lista: nossa classe Lista atende as principais
+# operações que essa TAD nos oferece, mas que tal melhorarmos? Para isso,
+# você deve adicionar os seguintes métodos:
+
+# a. A operação clear nos permite remover todos os Nodes da lista;
+
+#  b. A operação __get_node_at nos permite acessar o Node em qualquer posição
+# da lista.
+
+# Após criada as operações anteriores, refatore os seguintes métodos para que
+# utilizem a __get_node_at ante iterações:
+
+# insert_at;
+# insert_last;
+# remove_last;
+# remove_at;
+# get_element_at.
+
